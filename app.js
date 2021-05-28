68990dea3046e4968c430acaf5c99d0307dc006f
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-05-28 21:00:27 +0530

    Intro To Middlewares

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Something in the rain");
-    next();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size" value="1"><button type="submit">Add Product</button></form>') ;
 });
 
-app.use((req, res, next) => {
-    console.log("Something in the winter");
-    res.send( { key1: "value" }) ;
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-const server = http.createServer(app);
+app.use('/',(req, res, next) => {
+    res.send('<h1> hello to node js </h1>') ;
+});
 
-server.listen(4000);
+app.listen(4000);
