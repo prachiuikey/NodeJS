3f534bf8f490346b69accf7d60c21c71fcd04b2b
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-05-30 01:03:03 +0530

    Add files via upload

---
+++ add product/routes/contact.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/contact', (req,res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact.html'));
+});
+
+module.exports = router;
