d88ba68d618e291cba0a6f3ebfe8d4f038aeb8da
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-05-31 19:05:38 +0530

    Update contact.js

--- add product/routes/contact.js
+++ add product/routes/contact.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 
 const path = require('path');
@@ -9,5 +10,20 @@ const router = express.Router();
 router.get('/contact', (req,res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'contact.html'));
 });
+
+module.exports = router;
+*/
+
+// After adding controllers
+
+const express = require('express');
+
+const path = require('path');
+
+const contactControllers = require('../Controllers/contact');
+
+const router = express.Router();
+
+router.get('/contact', contactControllers.getContact);
 
 module.exports = router;
