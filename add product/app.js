087c9a9685df0a5ef665361dbb2fb587646d50e3
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-05-31 18:57:06 +0530

    Update app.js

--- add product/app.js
+++ add product/app.js
@@ -1,3 +1,4 @@
+/*
 const path = require('path');
 
 const rootDir = require('./util/path')
@@ -24,5 +25,39 @@ app.use(successRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
+
+app.listen(4000);
+*/
+
+
+const path = require('path');
+
+const rootDir = require('./util/path')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const errorController = require('./Controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes = require('./routes/success');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(rootDir, 'public')));
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+app.use(errorController.get404);
+
 
 app.listen(4000);
