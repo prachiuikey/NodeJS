38c0d404c81c23a27bed771e67d7cebb4e08a40d
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-05-31 19:06:34 +0530

    Update shop.js

--- add product/routes/shop.js
+++ add product/routes/shop.js
@@ -1,3 +1,4 @@
+/*
 const path = require('path');
 
 const rootDir = require('../util/path');
@@ -10,5 +11,18 @@ router.get('/',(req, res, next) => {
     res.sendFile(path.join(rootDir,'views','shop.html')) ;
 });
 
+
+module.exports = router;
+*/
+
+const path = require('path');
+
+const productControllers = require('../Controllers/Products');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', productControllers.addProducts);
 
 module.exports = router;
