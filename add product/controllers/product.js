c111c03c791d8620f174f4743b78af25a9c37df5
prachiuikey <52187509+prachiuikey@users.noreply.github.com>
2021-06-03 00:49:47 +0530

    Update product.js

--- add product/controllers/product.js
+++ add product/controllers/product.js
@@ -1,27 +1,53 @@
-const products = [];
+const Product = require('../models/product');
 
-exports.getAddProducts = (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-      });
-}
-
-exports.postAddProducts = (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
-}
-
-exports.addProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
   });
-}
+};
+
+exports.getProduct = (req,res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product : product,
+      pageTitle : product.title,
+      path : '/products'
+    })
+  })
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
